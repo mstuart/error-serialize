91491da5aba5ea5c7e3946556e7d801c72f3fc1f
Mark Stuart <mstuart@users.noreply.github.com>
2026-08-09 21:09:19 -0700

    chore: enforce ultracite

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,16 +1,16 @@
-import {expectType, expectError} from 'tsd';
-import {serialize, deserialize, type SerializedError} from './index.js';
+import { expectError, expectType } from "tsd";
+import { deserialize, type SerializedError, serialize } from "./index.js";
 
-const serialized = serialize(new Error('test'));
+const serialized = serialize(new Error("test"));
 expectType<SerializedError>(serialized);
 
-const deserialized = deserialize({name: 'Error', message: 'test'});
+const deserialized = deserialize({ message: "test", name: "Error" });
 expectType<Error>(deserialized);
 
-const unknown_ = serialize('not an error');
+const unknown_ = serialize("not an error");
 expectType<unknown>(unknown_);
 
-const unknownDeser = deserialize('not an object');
+const unknownDeser = deserialize("not an object");
 expectType<unknown>(unknownDeser);
 
 expectError(serialize());
