b97ee0c6d6856cb06aa46360b1122933ceae8f18
Mark Stuart <mstuart@users.noreply.github.com>
2026-08-09 22:39:24 -0700

    chore: migrate to Ultracite (#10)
    * chore: migrate to ultracite
    
    * chore: enforce ultracite
    
    ---------
    
    Co-authored-by: Mark Stuart <mstuart@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,70 +1,75 @@
 const errorConstructors = {
-	Error,
-	TypeError,
-	RangeError,
-	ReferenceError,
-	SyntaxError,
-	URIError,
-	EvalError,
+  Error,
+  EvalError,
+  RangeError,
+  ReferenceError,
+  SyntaxError,
+  TypeError,
+  URIError,
 };
 
 function getErrorConstructor(name) {
-	return errorConstructors[name] ?? Error;
+  return errorConstructors[name] ?? Error;
 }
 
 export function serialize(error) {
-	if (!(error instanceof Error)) {
-		return error;
-	}
+  if (!(error instanceof Error)) {
+    return error;
+  }
 
-	const serialized = {
-		name: error.name,
-		message: error.message,
-		stack: error.stack,
-	};
+  const serialized = {
+    message: error.message,
+    name: error.name,
+    stack: error.stack,
+  };
 
-	if (error.code !== undefined) {
-		serialized.code = error.code;
-	}
+  if (error.code !== undefined) {
+    serialized.code = error.code;
+  }
 
-	if (error.cause !== undefined) {
-		serialized.cause = serialize(error.cause);
-	}
+  if (error.cause !== undefined) {
+    serialized.cause = serialize(error.cause);
+  }
 
-	for (const key of Object.keys(error)) {
-		if (!(key in serialized)) {
-			serialized[key] = error[key];
-		}
-	}
+  for (const key of Object.keys(error)) {
+    if (!(key in serialized)) {
+      serialized[key] = error[key];
+    }
+  }
 
-	return serialized;
+  return serialized;
 }
 
 export function deserialize(object) {
-	if (typeof object !== 'object' || object === null || !object.name || !object.message) {
-		return object;
-	}
+  if (
+    typeof object !== "object" ||
+    object === null ||
+    !object.name ||
+    !object.message
+  ) {
+    return object;
+  }
 
-	const ErrorConstructor = getErrorConstructor(object.name);
-	const error = new ErrorConstructor(object.message);
+  const ErrorConstructor = getErrorConstructor(object.name);
+  const error = new ErrorConstructor(object.message);
 
-	if (object.stack !== undefined) {
-		error.stack = object.stack;
-	}
+  if (object.stack !== undefined) {
+    error.stack = object.stack;
+  }
 
-	if (object.code !== undefined) {
-		error.code = object.code;
-	}
+  if (object.code !== undefined) {
+    error.code = object.code;
+  }
 
-	if (object.cause !== undefined) {
-		error.cause = deserialize(object.cause);
-	}
+  if (object.cause !== undefined) {
+    error.cause = deserialize(object.cause);
+  }
 
-	for (const key of Object.keys(object)) {
-		if (!['name', 'message', 'stack', 'code', 'cause'].includes(key)) {
-			error[key] = object[key];
-		}
-	}
+  for (const key of Object.keys(object)) {
+    if (!["name", "message", "stack", "code", "cause"].includes(key)) {
+      error[key] = object[key];
+    }
+  }
 
-	return error;
+  return error;
 }
